56f24c92e14f9f12d77f34196b83fe2df6d45b75
Dilshodaa <100948868+Dilshodaa@users.noreply.github.com>
2022-03-11 11:47:38 +0500

    kalkulator

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+var a = prompt('Birinchi sonni kiriting')
+var b = prompt('Amallardan birini kiriting: "+", "-", "*", "/" "%"')
+var c = prompt('2chi  sonni kiriting')
+document.write('<h1>' + a + '  ' + b + '  ' + c + '  ' + ' = ' + eval (a + b + c))
